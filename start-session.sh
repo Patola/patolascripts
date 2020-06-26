70d3d55992952ac8e76a5b2bf83c89f389a00926
Cláudio 'Patola' Sampaio <patola@gmail.com>
2020-06-26 02:56:14 +0200

    relativized performance start

--- start-session.sh
+++ start-session.sh
@@ -18,13 +18,18 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-/usr/bin/pacmd set-default-sink alsa_output.pci-0000_22_00.1.hdmi-stereo-extra1
+#/usr/bin/pacmd set-default-sink alsa_output.pci-0000_22_00.1.hdmi-stereo-extra1
 
 # FULL SPEED AHEAD!
-for a in {0..15}
+#for a in {0..15}
+#do
+#  sudo cpufreq-set -c $a -g performance
+#done
+for a in {1..$(lscpu | awk '($1=="CPU(s):") {print $2}')}
 do
-  sudo cpufreq-set -c $a -g performance
+  sudo cpufreq-set -c $((a-1)) -g performance
 done
+
 
 #NVIDIA: Force full composition pipeline
 /usr/bin/nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 { ForceFullCompositionPipeline = On }"
@@ -32,6 +37,6 @@ done
 /home/patola/bin/nvidia-performance.sh
 
 #/usr/bin/pacmd set-default-sink alsa_output.usb-045e_Microsoft_LifeChat_LX-3000-00.iec958-stereo
-/usr/bin/xgamma -gamma 1.2
+#/usr/bin/xgamma -gamma 1.2
 ~/bin/teclado.sh
 /usr/bin/xset s 3600
