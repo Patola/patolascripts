ce1880368841ba25ab48cf150bdf450aeb69618c
Cláudio 'Patola' Sampaio <patola@gmail.com>
2020-12-05 12:25:11 +0100

    Added verification against multiple instances

--- resteam.sh
+++ resteam.sh
@@ -18,11 +18,12 @@
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
+ps -ef | grep resteam | grep -v grep | awk -v PR=$$ '($2!=PR) {print $0}' | grep resteam >/dev/null 2>&1  && exit 1
 while :
 do
   sleep 30
   ps -ef | grep -v grep | grep -v dumps/assert | grep -v resteam.sh | grep steam > /dev/null 2>&1 || launch /usr/games/steam
-  z="$(ps -ef | grep -v grep | grep steam | grep -v steamwebhelper | grep -v fossilize_replay | grep -v dumps/assert | sort -n -k 7 | tail -1 | awk '{print $7}')"; [[ "$z" =~ [0-9][0-9]:[0-9][0-9]:[0-9][0-9] && "$z" > "00:30:00" ]] && break
+  z="$(ps -ef | grep -v grep | grep steam | grep -v steamwebhelper | grep -v fossilize_replay | grep -v dumps/assert | sort -n -k 7 | tail -1 | awk '{print $7}')"; [[ "$z" =~ [0-9][0-9]:[0-9][0-9]:[0-9][0-9] && "$z" > "00:10:00" ]] && break
 done
 
 exit 0
